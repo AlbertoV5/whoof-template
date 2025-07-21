a944846f3946d83f84759d34b84290b94a0a172e
Alberto Valdez <avq5ac1@gmail.com>
2025-07-20 19:19:01 -0600

    use layout nextjs

---
+++ app/experiences/[experienceId]/layout.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { getAuthenticatedUser } from '@/lib/auth';
+import { whopSdk } from '@/lib/whop-sdk';
+import { AppBuilder } from '@whoof/ui';
+import { env } from '@/lib/env';
+import 'frosted-ui/styles.css';
+
+export default async function ExperiencePage({
+	children,
+	params
+}: {
+	children: React.ReactNode
+	params: Promise<{ experienceId: string }>
+}) {
+	return <AppBuilder
+		params={params}
+		whopSdk={whopSdk}
+		appView={{
+			user: ({ user, experience }) => (<div>User {user.userId} {experience.id}</div>),
+			creator: ({ user, experience }) => (<div>Creator {user.userId} {experience.id}</div>),
+			developer: ({ user, experience }) => (<div>Developer {user.userId} {experience.id}</div>),
+		}}
+		appConfig={{
+			appId: env.NEXT_PUBLIC_WHOP_APP_ID,
+		}}
+		getUser={getAuthenticatedUser}
+		fetchData={async ({ user, experience }) => {
+			console.log('🔍 User:', user)
+			console.log('🔍 Experience:', experience)
+			try {
+				return {}
+			} catch (error) {
+				throw error
+			}
+		}}>
+		<>{children}</>
+	</AppBuilder>
+}
+
